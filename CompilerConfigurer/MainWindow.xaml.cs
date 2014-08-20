3a53573b9dc6b1ab926bbd7d4ea7ad4199b96ec3
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2014-08-20 14:42:53 -0400

    Added Initial Projects

---
+++ CompilerConfigurer/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace CompilerConfigurer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const string CONFIG_FILE_PATH = "config";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if(File.Exists(CONFIG_FILE_PATH))
+            {
+                var fileContents = File.ReadAllLines(CONFIG_FILE_PATH);
+                vbspLocation.Text = TryGetLine(fileContents, 1);
+                vbspParameters.Text = TryGetLine(fileContents, 3);
+                vradLocation.Text = TryGetLine(fileContents, 5);
+                vradParameters.Text = TryGetLine(fileContents, 7);
+                vvisLocation.Text = TryGetLine(fileContents, 9);
+                vvisParameters.Text = TryGetLine(fileContents, 11);
+                bspDirectory.Text = TryGetLine(fileContents, 13);
+            }
+            SaveButton.IsEnabled = false;
+        }
+
+        private string TryGetLine(string[] strings, int line)
+        {
+            return strings.Length > line ? 
+                strings[line] :
+                string.Empty;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllLines(CONFIG_FILE_PATH,new string[]{
+                "vbsp.exe Path:",  vbspLocation.Text,
+                "vbsp.exe Parameters:", vbspParameters.Text,
+                "vrad.exe Path:", vradLocation.Text,
+                "vrad.exe Parameters:", vradParameters.Text,
+                "vvis.exe Path:", vvisLocation.Text,
+                "vvis.exe Parameters:", vvisParameters.Text,
+                "Output directory (make sure ends with \\):", bspDirectory.Text,
+                "","","", "Don't change the above unless you know what you're doing... These bad boys are hard coded to read from specific lines, so keep the compiler path and parameters right where they are"
+            });
+            SaveButton.IsEnabled = false;
+        }
+
+        private void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SaveButton.IsEnabled = true;
+        }
+    }
+}
---
+++ MassCompiler/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace MassCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            args = new string[] { "dev_room.vmf" };
+#endif
+
+            string vbspPath=string.Empty;
+            string vbspParams = string.Empty;
+            string vradPath = string.Empty;
+            string vradParams = string.Empty;
+            string vvisPath = string.Empty;
+            string vvisParams = string.Empty;
+            string outputdirectory = string.Empty;
+
+            //Above, default values are given incase we don't get info from our config file
+
+            if (File.Exists("config"))
+            {
+                var config = File.ReadAllLines("config");
+                if (config.Length > 1 && !string.IsNullOrWhiteSpace(config[1])) vbspPath = config[1];
+                if (config.Length > 3 && !string.IsNullOrWhiteSpace(config[3])) vbspParams = config[3];
+                if (config.Length > 5 && !string.IsNullOrWhiteSpace(config[5])) vradPath = config[5];
+                if (config.Length > 7 && !string.IsNullOrWhiteSpace(config[7])) vradParams = config[7];
+                if (config.Length > 9 && !string.IsNullOrWhiteSpace(config[9])) vvisPath = config[9];
+                if (config.Length > 11 && !string.IsNullOrWhiteSpace(config[11])) vvisParams = config[11];
+                if (config.Length > 13 && !string.IsNullOrWhiteSpace(config[13])) outputdirectory = config[13];
+            }
+            else
+            {
+                Console.WriteLine("No config found");
+                return;
+            }
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                DateTime start = DateTime.Now;
+                try
+                {
+                    string filename = args[i].Replace(".vmf", "");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Compiling vmf {0} / {1} ({2})", i + 1, args.Length, filename);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Process proc;
+
+                    Console.WriteLine("Running VBSP...");
+                    Console.ForegroundColor = ConsoleColor.White; Console.Write("vbsp.exe "); 
+                    Console.ForegroundColor = ConsoleColor.Cyan; Console.WriteLine(string.Format(vbspParams, filename)); 
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    proc = Process.Start(new ProcessStartInfo(vbspPath, string.Format(vbspParams, filename)) { UseShellExecute = false, RedirectStandardOutput = true });
+                    Console.WriteLine(proc.StandardOutput.ReadToEnd());
+                    proc.WaitForExit();
+                    Console.WriteLine("Finished.");
+
+                    Console.WriteLine("Running VVIS...");
+                    Console.ForegroundColor = ConsoleColor.White; Console.Write("vvis.exe ");
+                    Console.ForegroundColor = ConsoleColor.Cyan; Console.WriteLine(string.Format(vvisParams, filename));
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    proc = Process.Start(new ProcessStartInfo(vvisPath, string.Format(vvisParams, filename)) { UseShellExecute = false, RedirectStandardOutput = true });
+                    Console.WriteLine(proc.StandardOutput.ReadToEnd());
+                    proc.WaitForExit();
+                    Console.WriteLine("Finished.");
+
+                    Console.WriteLine("Running VRAD...");
+                    Console.ForegroundColor = ConsoleColor.White; Console.Write("vrad.exe ");
+                    Console.ForegroundColor = ConsoleColor.Cyan; Console.WriteLine(string.Format(vradParams, filename));
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    proc = Process.Start(new ProcessStartInfo(vradPath, string.Format(vradParams, filename)) { UseShellExecute = false, RedirectStandardOutput = true });
+                    Console.WriteLine(proc.StandardOutput.ReadToEnd());
+                    proc.WaitForExit();
+                    Console.WriteLine("Finished.");
+
+                    //string bspName = args[i].Replace(".vmf", ".bsp");
+                    //if (!Directory.Exists(outputdirectory))
+                    //    Directory.CreateDirectory(outputdirectory);
+                    //File.Copy(bspName, outputdirectory + bspName);
+                }
+                catch (Exception ex)
+                {
+                    ConsoleColor saveit = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("There was an exception");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine(ex.ToString());
+                    Console.ForegroundColor = saveit;
+                }
+                finally
+                {
+                    DateTime end = DateTime.Now;
+                    Console.WriteLine("Finished. Took {0} sconds.", end - start);
+                }
+            }
+
+            Console.WriteLine("All done, press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
