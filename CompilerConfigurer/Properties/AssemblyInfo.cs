b69ff68d66f015e5167949337f355286b6ff30c7
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2014-08-21 18:40:52 -0400

    Update assembly info

--- CompilerConfigurer/Properties/AssemblyInfo.cs
+++ CompilerConfigurer/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CompilerConfigurer")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2014 Benjamin Thomas Blodgett")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
